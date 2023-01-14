8a952255e34b91a86ca6899727b227b1848723fa
Jmanz0 <jason.manzara@gmail.com>
2023-01-14 10:01:05 -0800

    test

--- src/App.js
+++ src/App.js
@@ -18,14 +18,19 @@ function App() {
       <a-assets>
         <img id= "sky" src={sky} />
       </a-assets>
-
+      <a-entity id="mouseCursor" cursor="rayOrigin: mouse"></a-entity>
+      <a-entity 
+      animation="property: visible; to: true; startEvents: mouseclick;"
+      position="-2.5 1 0"
+                text="anchor: left; width: 10; color: black; value: [LEFT ANCHOR] Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut enim ad minim veniam">
+      </a-entity>
       <a-sky
         color = "#FFFFFF"
         material="src: #sky"
         rotation="0 0 0"
       >
       </a-sky>
-      <a-entity id="store" position="-200 -12 170" scale="12 12 12"></a-entity>
+      {/* <a-entity id="store" position="-200 -12 170" scale="12 12 12" color="black"></a-entity> */}
       <CropHoodie x={0} y={-22} z={0}/>
     </a-scene> 
   );
