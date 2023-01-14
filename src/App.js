fefa0271c31b8e61c779eed9a223216e147f62b3
Jmanz0 <jason.manzara@gmail.com>
2023-01-14 12:04:23 -0800

    no idea if anything changed

--- src/App.js
+++ src/App.js
@@ -20,38 +20,40 @@ function App() {
   })
 
   return (
-    <a-scene>
-      <a-assets>
-        <img id= "ceiling" src={ceiling} />
-      </a-assets>
+    <div>
+      <a-scene>
+        <a-assets>
+          <img id= "ceiling" src={ceiling} />
+        </a-assets>
+        <a-entity
+            position="-34 10 -40"
+            text="font: mozillavr; anchor: left; width: 20; color: white; value: CropHoodie: \n Made with real cotton">
+            
+      </a-entity>
       <a-entity
-          position="-34 10 -40"
-          text="font: mozillavr; anchor: left; width: 15; color: white; value: CropHoodie: \n Made with real cotton">
-          
-     </a-entity>
-     <a-entity
-          position="-8 10 -40"
-          text="font: mozillavr; anchor: left; width: 15; color: white; value: Dress: \n Made with real cotton">
-     </a-entity>
-     <a-entity
-          position="35 10 -40"
-          text="font: mozillavr; anchor: left; width: 15; color: white; value: Sweatpants: \n Made with real cotton">
-     </a-entity>
+            position="-8 10 -40"
+            text="font: mozillavr; anchor: left; width: 20; color: white; value: Dress: \n Made with real cotton">
+      </a-entity>
+      <a-entity
+            position="35 10 -40"
+            text="opacity: 1; font: mozillavr; anchor: left; width: 20; color: white; value: Sweatpants: \n Made with real cotton">
+      </a-entity>
 
 
-      <a-sky
-        color = "#FFFFFF"
-        material="src: #ceiling"
-        rotation="0 0 0"
-      >
-      </a-sky>
-      <a-entity id="store" position="-230 -12 130" scale="12 12 12"></a-entity>
-      <CropHoodie x={-30} y={-22} z={-40}/>
-      <Dress x={-6} y={-9} z={-40}/>
-      <SecondDress x={6} y={-20} z={-40}/>
-      <Jeans x={50} y={-15} z={-40}/>
-      <Sweatpants x={40} y={-10} z={-40}/>o
-    </a-scene> 
+        <a-sky
+          color = "#FFFFFF"
+          material="src: #ceiling"
+          rotation="0 0 0"
+        >
+        </a-sky>
+        <a-entity id="store" position="-230 -12 130" scale="12 12 12"></a-entity>
+        <CropHoodie x={-30} y={-22} z={-40}/>
+        <Dress x={-6} y={-9} z={-40}/>
+        <SecondDress x={6} y={-20} z={-40}/>
+        <Jeans x={50} y={-15} z={-40}/>
+        <Sweatpants x={40} y={-10} z={-40}/>o
+      </a-scene> 
+    </div>
   );
 }
 
