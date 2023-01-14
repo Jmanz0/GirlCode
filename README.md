a8c22f5d3a3a0c3baf4710300299cccc8fe5f55c
Jmanz0 <jason.manzara@gmail.com>
2023-01-13 21:20:01 -0800

    first commit

--- README.md
+++ README.md
@@ -71,3 +71,5 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/t
 # GirlCode
 # GirlCode
 # GirlCode
+# GirlCode
+# GirlCode
---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import 'aframe';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+import sky from './stars.jpg';
+import mountain from './models/mountain.glb';
+import Rocket from './Rocket';
+
+function App() {
+  const loader  = new GLTFLoader();
+
+  loader.load(mountain, (d) =>{
+    const entity = document.getElementById("mountain");
+    entity.object3D.add(d.scene);
+  })
+
+  return (
+    <a-scene>
+      <a-assets>
+        <img id= "sky" src={sky} />
+      </a-assets>
+
+      <a-sky
+        color = "#FFFFFF"
+        material="src: #sky"
+        rotation="0 0 0"
+      >
+      </a-sky>
+      <a-entity id="mountain" position="0 -12 0" scale="12 12 12"></a-entity>
+      <Rocket x={0} y={50} z={-80}/>
+    </a-scene>
+  );
+}
+
+export default App;
