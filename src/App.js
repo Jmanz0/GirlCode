1b77007c637af2f1eb389a346c726546d5fa806e
Carolyn <carolynhuang15@gmail.com>
2023-01-14 09:58:30 -0800

    Added more items to the store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import 'aframe';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
-import sky from './white.jpg';
+import ceiling from './white.jpg';
 import store from './models/store.glb';
-import CropHoodie from './CropHoodie';
+import CropHoodie from './partials/CropHoodie';
+import Dress from './partials/Dress';
+import SecondDress from './partials/SecondDress';
+import Jeans from './partials/Jeans';
+import Sweatpants from './partials/Sweatpants';
+
 
 function App() {
   const loader  = new GLTFLoader();
@@ -16,17 +21,21 @@ function App() {
   return (
     <a-scene>
       <a-assets>
-        <img id= "sky" src={sky} />
+        <img id= "ceiling" src={ceiling} />
       </a-assets>
 
-      <a-sky
+      <a-ceiling
         color = "#FFFFFF"
-        material="src: #sky"
+        material="src: #ceiling"
         rotation="0 0 0"
       >
-      </a-sky>
+      </a-ceiling>
       <a-entity id="store" position="-200 -12 170" scale="12 12 12"></a-entity>
       <CropHoodie x={0} y={-22} z={0}/>
+      <Dress x={-180} y={-22} z={0}/>
+      <SecondDress x={-165} y={-22} z={0}/>
+      <Jeans x={-220} y={-22} z={0}/>
+      <Sweatpants x={-235} y={-22} z={0}/>
     </a-scene> 
   );
 }
---
+++ src/partials/Dress.js
@@ -0,0 +1,27 @@
+import 'aframe';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+import dressModel from './models/dress.glb';
+import Prototypes from 'prop-types';
+
+
+function Dress({x, y, z}) {
+    const loader = new GLTFLoader();
+    loader.load(dressModel, (d) => {
+        const entity = document.getElementById('dress');
+        entity.object3D.add(d.scene);
+    })
+
+    return(
+        <a-entity id="dress" position={`${x} ${y} ${z}`} scale="15 15 15" rotation="0 0 0" 
+        animation="property:rotation; from: 0 0 0; to: 0 360 0; loop: true; dur: 10000"></a-entity>
+    );
+}
+
+Dress.Prototypes = {
+    x: Prototypes.number,
+    y: Prototypes.number,
+    z: Prototypes.number
+}
+
+
+export default Dress;
